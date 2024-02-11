aeddf98a9a9df94d6b638f0f4ada494265615893
dhananjay8968 <dhananjaymahtha29@gmail.com>
2024-02-11 19:22:12 +0530

    return syntax fix user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -223,7 +223,7 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
 const getCurrentUser = asyncHandler(async(req,res) => {
     return res
     .status(200)
-    .json(200,req.user,"current user fetched")
+    .json(new ApiResponse(200,req.user,"current user fetched"))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res) => {
@@ -231,7 +231,7 @@ const updateAccountDetails = asyncHandler(async(req,res) => {
     if(!(fullName || email)) {
         throw new ApiError(400,"All fields are required")
     }
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
